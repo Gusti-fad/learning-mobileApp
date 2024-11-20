ca9d83ba5eb5378a183147ad5e582740d853010b
unknown <gfadhillah06@gmail.com>
2024-11-20 10:55:18 +0700

    add link

--- app/about.tsx
+++ app/about.tsx
@@ -1,8 +1,25 @@
-import {View, Text} from 'react-native';
+import { Link } from 'expo-router';
+import {View, Text, StyleSheet} from 'react-native';
 export default function About() {
     return (
-        <View>
-            <Text>About</Text>
+        <View style={styles.container}>
+            <Text style={styles.text}>About</Text>
+            <Link style={styles.link} href="/">Home</Link>
         </View>
     );
 }
+const styles = StyleSheet.create({
+    container : {
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+        backgroundColor: "black",
+    },
+    text: {
+        color: "white",
+    },
+    link: {
+        color: "white",
+        textDecorationLine: "underline",
+    }
+})
--- app/index.tsx
+++ app/index.tsx
@@ -1,9 +1,11 @@
+import { Link } from "expo-router";
 import { Text, View, StyleSheet } from "react-native";
 
 export default function Index() {
   return (
     <View style={styles.container}>
       <Text style={styles.text}>Hello World!</Text>
+      <Link style={styles.text} href={"/about"}>About</Link>
     </View>
   );
 }
